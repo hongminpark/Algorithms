fd64ddf612217719a6ea47468e36d8b9bea29987
hongminpark <zhdals1026@gmail.com>
2018-05-23 00:40:41 +0900

    defs 'remove()' and 'removeItem()' merged

--- LinkedList.py
+++ LinkedList.py
@@ -1,3 +1,6 @@
+# Code referenced from https://github.com/minsuk-heo/problemsolving/blob/master/data_structure/LinkedList.py
+
+
 class Node:
     def __init__(self, item):
         self.val = item
@@ -30,21 +33,34 @@ class LinkedList:
 
             # First, find item from the list
             cur = self.head
-            while cur.next is not None:
+            while cur is not None:
+                if cur.next.val is item:
+                    cur.next = cur.next.next
+                    break
                 cur = cur.next
-                if cur.val is item:
-                    self.removeItem(item)
-                    return
-            print("Item doesn't exist in this Linked List")
 
-    def removeItem(self, item):
-        cur = self.head
-        while cur is not None:
-            if cur.next.val is item:
-                cur.next = cur.next.next
-                break
-            cur = cur.next
-    # Remove and RemoveItem. Could I merge those two?
+    # def remove(self, item):
+    #     if self.head.val is item:
+    #         self.head = self.head.next
+    #     else:
+    #
+    #         # First, find item from the list
+    #         cur = self.head
+    #         while cur.next is not None:
+    #             cur = cur.next
+    #             if cur.val is item:
+    #                 self.removeItem(item)
+    #                 return
+    #         print("Item doesn't exist in this Linked List")
+    #
+    # def removeItem(self, item):
+    #     cur = self.head
+    #     while cur is not None:
+    #         if cur.next.val is item:
+    #             cur.next = cur.next.next
+    #             break
+    #         cur = cur.next
+    # # Remove and RemoveItem. Could I merge those two?
 
     def reverse(self):
         prev = None
@@ -67,5 +83,5 @@ if __name__ == "__main__":
     l_list.add(4)
     l_list.reverse()
     l_list.print_()
-    l_list.remove(5)
+    l_list.remove(2)
     l_list.print_()
