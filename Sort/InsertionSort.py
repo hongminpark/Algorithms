fd364681ebfd1db13a971e93d7b5ead13771f985
hongminpark <zhdals1026@gmail.com>
2019-10-09 11:00:55 +0900

    Refactor InsertionSort

--- Sort/InsertionSort.py
+++ Sort/InsertionSort.py
@@ -1,19 +1,19 @@
-def insertion_sort(list):
-    for i in range(len(list)):
-        # set each item an insertion value
-        tmp_min = list[i]
-
-        if i is not 0:
-            # compare the insertion location from the sorted list
-            for idx, item in enumerate(list[:i]):
-                if tmp_min < item:
-                    # is it meaningful? 
-                    list.remove(tmp_min)
-                    list.insert(idx, tmp_min)
-                    break
+def insertion_sort(A):
+    """
+    [idea]
+    1. i번째 아이템을 정렬되어있는 i-1번째 까지의 리스트와 비교해서 적절할 위치에 넣는다.
+    2. 0번째부터 탐색하면 i를 탐색할 때 i-1번째까지의 리스트는 항상 정렬되어 있다.
+    """
+    for i, a in enumerate(A):
+        if not i:
+            continue
+        for i_, a_ in enumerate(A[:i]):
+            if a <= a_:
+                A[i_:i+1] = [a] + A[i_:i]
+                break
+    return A
 
 
 if __name__ == "__main__":
-    test_list = [6, 4, 10, 1, 2, 5]
-    insertion_sort(test_list)
-    print(test_list)
+    A = [1, 5, 34, 4, 2, 4, 3, 21, 205, 3]
+    print(insertion_sort(A))
