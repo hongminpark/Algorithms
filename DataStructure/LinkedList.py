19ad06b31375c5453ccfcfc80487fea81bf25141
hongminpark <zhdals1026@gmail.com>
2019-10-13 19:27:25 +0900

    Refactor LinkedList.py

--- DataStructure/LinkedList.py
+++ DataStructure/LinkedList.py
@@ -1,87 +1,64 @@
-# Code referenced from https://github.com/minsuk-heo/problemsolving/blob/master/data_structure/LinkedList.py
-
-
 class Node:
     def __init__(self, item):
-        self.val = item
+        self.value = item
         self.next = None
 
 
 class LinkedList:
+
     def __init__(self):
         self.head = None
 
     def add(self, item):
-        if self.head is None:
-            self.head = Node(item)
-        else:
+        if self.head:
             cur = self.head
-            while cur.next is not None:
+            while cur.next:
                 cur = cur.next
             cur.next = Node(item)
-
-    def print_(self):
-        cur = self.head
-        while cur is not None:
-            print(cur.val)
-            cur = cur.next
+        else:
+            self.head = Node(item)
 
     def remove(self, item):
-        if self.head.val is item:
+        if self.head.value == item:
             self.head = self.head.next
         else:
-
-            # First, find item from the list
             cur = self.head
-            while cur is not None:
-                if cur.next.val is item:
+            while cur.next:
+                if cur.next.value == item:
                     cur.next = cur.next.next
                     break
                 cur = cur.next
 
-    # def remove(self, item):
-    #     if self.head.val is item:
-    #         self.head = self.head.next
-    #     else:
-    #
-    #         # First, find item from the list
-    #         cur = self.head
-    #         while cur.next is not None:
-    #             cur = cur.next
-    #             if cur.val is item:
-    #                 self.removeItem(item)
-    #                 return
-    #         print("Item doesn't exist in this Linked List")
-    #
-    # def removeItem(self, item):
-    #     cur = self.head
-    #     while cur is not None:
-    #         if cur.next.val is item:
-    #             cur.next = cur.next.next
-    #             break
-    #         cur = cur.next
-    # # Remove and RemoveItem. Could I merge those two?
-
     def reverse(self):
-        prev = None
+        before = None
         cur = self.head
-        while cur is not None:
-            tmp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = tmp
-        self.head = prev
+        while cur:
+            next = cur.next
+            cur.next = before
+            before = cur
+            cur = next
+        self.head = before
+
+    def print(self):
+        result = []
+        cur = self.head
+        while cur:
+            result.append(cur.value)
+            cur = cur.next
+        print(result)
 
 
 if __name__ == "__main__":
-    l_list = LinkedList()
-    l_list.add(3)
-    l_list.add(5)
-    l_list.add(8)
-    l_list.add(2)
-    l_list.add(9)
-    l_list.add(4)
-    l_list.reverse()
-    l_list.print_()
-    l_list.remove(2)
-    l_list.print_()
+    linkedlist = LinkedList()
+    linkedlist.add(3)
+    linkedlist.add(5)
+    linkedlist.add(2)
+    linkedlist.add(7)
+    linkedlist.add(5)
+    linkedlist.add(7)
+    linkedlist.add(9)
+    linkedlist.reverse()
+    linkedlist.print()
+    linkedlist.remove(7)
+    linkedlist.print()
+
